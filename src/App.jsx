e7a54070c796897ff6799a5598a651f2b62c8247
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-07-04 18:34:50 +0600

    Creating the VanDetails Component & Corresponding Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import About from './pages/About.jsx'
 import Home from './pages/Home.jsx'
 import Footer from './pages/Footer.jsx'
+import Vans from './pages/Vans.jsx'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './index.css'
 import logo from './assets/logog.svg'
+import './server.js'
+import VanDetails from './pages/VanDetails.jsx'
 
 export default function App() {
+  console.log(Routes)
   return (
       <BrowserRouter>
         <header>
@@ -14,11 +18,14 @@ export default function App() {
           </Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/About" element={<About />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />}/>
+          <Route path="/vans/:id" element={<VanDetails />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
